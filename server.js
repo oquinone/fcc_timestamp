07ef7dfcfc86c5f21fe23f179d4463edda2d7a0a
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-04-12 15:17:56 -0700

    Uncommented needed code

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ process.env.PORT=3000;
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-// app.use(express.static('public'));
+http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-// app.get("/", function (req, res) {
-//   res.sendFile(__dirname + '/views/index.html');
-// });
+http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 
 // your first API endpoint... 
